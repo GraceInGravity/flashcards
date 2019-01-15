aa8b1d203eb7f12a6b8ca5c6985d3ecb62e8dce2
nuala gray and nathan nielson <nathan@>
2019-01-15 15:11:35 -0800

    fix formatting, making the toggle button work, add content

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,45 @@
 // js file for flashcards
 
 $(Document).ready(function() {
+
     $("#card1").click(function(){
      $("#click1").fadeToggle("fast");
     });
+
+    $("#card2").click(function(){
+     $("#click2").fadeToggle("fast");
+    });
+
+    $("#card3").click(function(){
+     $("#click3").fadeToggle("fast");
+    });
+
+    $("#card4").click(function(){
+     $("#click4").fadeToggle("fast");
+    });
+
+    $("#card5").click(function(){
+     $("#click5").fadeToggle("fast");
+    });
+
+    $("#card6").click(function(){
+     $("#click6").fadeToggle("fast");
+    });
+
+    $("#card7").click(function(){
+     $("#click7").fadeToggle("fast");
+    });
+
+    $("#card8").click(function(){
+     $("#click8").fadeToggle("fast");
+    });
+
+    $("#card9").click(function(){
+     $("#click9").fadeToggle("fast");
+    });
+
+
+
 });
 //
 //     $(".card2").click(function(){
