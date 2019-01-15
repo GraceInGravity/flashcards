a4c17d6241593b10294b89459dcaf04672b1c678
nuala gray and nathan nielson <nuala@>
2019-01-15 14:37:31 -0800

    add first button and structure of columns and rows

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+// js file for flashcards
+
+$(Document).ready(function() {
+    $("#card1").click(function(){
+     $("#click1").fadeToggle("fast");
+    });
+});
+//
+//     $(".card2").click(function(){
+//      $("#inform2").fadeToggle("fast");
+//     });
+//
+//     $(".card3").click(function(){
+//      $("#inform3").fadeToggle("fast")
+//    });
+//     });
+//
+//
+//
+//
+//
+//       $("button#yellow").click(function() {
+//         $("body").addClass("TheVocabWord");
+//       });
+//
+//
+//       $("button#green").click(function() {
+//         $("body").addClass("wordarray[i]");
+//       });
+//
+//
+//
+//
+//     for (var i = 0; i < wordarray.length; i++) {
+//       wordarray[i]
+//
+// while (row<3) {(wordarray[i])
+//   while (col<4)
+//
+// }
+//
+//       definarray.length
+//
+//       wordarray[i] = [a,b,c,d,e]
+//       definarray[i] = [aa,bb,cc,dd,ee]
+//
+//
+//
+//
+//         word
+//     }
